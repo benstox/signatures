611ef418ee4f080c1d3097a27ca20499bcd0a516
benstox <benstox@gmail.com>
2017-09-04 10:19:23 +0100

    Hexavigesimal alphabet indexing of signatures.

--- src/signatures/core.clj
+++ src/signatures/core.clj
@@ -4,6 +4,37 @@
 (defn char-range [start end]
   (map char (range (int start) (inc (int end)))))
 
+(def A-start (int \A))
+
+(def alphabet-length (count (char-range \A \Z)))
+
+(defn char-to-hexavigesimal [character]
+  (mod (- (int character) A-start) alphabet-length))
+
+(defn hexavigesimal-to-char [xxvi]
+  (char (+ xxvi A-start)))
+
+(defn inc-hexavigesimal [xxvi-list]
+  (let [c (reverse xxvi-list)]
+    (if (zero? (mod (inc (first c)) alphabet-length))
+      (if (empty? (rest c))
+        [0 0] ;; add a hexavigesimal place
+        (reverse (cons 0 (reverse (inc-hexavigesimal (reverse (rest c))))))) ;; carry the one
+      (reverse (cons (inc (first c)) (rest c)))))) ;; simple increments
+
+(defn inc-chars [char-list]
+  (let [xxvi-list (map char-to-hexavigesimal char-list)]
+    (map
+      hexavigesimal-to-char
+      (inc-hexavigesimal xxvi-list))))
+
+(defn alphabet-index
+  ([]
+    (alphabet-index [\A]))
+  ([x]
+    (lazy-seq
+      (cons x (alphabet-index (inc-chars x))))))
+
 (defn parse-int [s]
   (Integer/parseInt (re-find #"\A-?\d+" s)))
 
@@ -15,14 +46,13 @@
         no-pages (count page-nos)
         no-sheets (int (Math/ceil (/ no-pages 4)))
         no-signatures (int (Math/ceil (/ no-pages 16)))
-        signatures (partition 16 16 nil page-nos)
-        alphabet (char-range \A \Z)]
+        signatures (partition 16 16 nil page-nos)]
     (println (str "Number of document pages to print: " no-pages))
     (println (str "Number of sheets to print: " no-sheets))
     (println (str "Number of 4-sheet signatures to bind: " no-signatures))
     (println "#####################################")
-    (doseq [[letter signature] (map vector alphabet signatures)]
-      (println (str "Signature " letter ". First page: " (first signature) ", last page: " (last signature))))
+    (doseq [[letters signature] (map vector (alphabet-index) signatures)]
+      (println (str "Signature " (apply str letters) ". First page: " (first signature) ", last page: " (last signature))))
     (println "#####################################")))
 
 (defn -main
